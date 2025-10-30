88e39e3b98c889c0d5716159b9d3bcd6dd7d901e
ikki yamanaka <ikki.yama@icloud.com>
2025-10-30 13:13:05 +0900

    Support integer calculation

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2025 ikki yamanaka
 # SPDX-License-Identifier: BSD-3-Clause 
 
@@ -11,7 +11,19 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
+++ seq 5
+++ ./plus
+
 [ "${out}" = 15 ] || ng "$LINENO"
++'[' 15 = 15']'
+
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
++ '[' 0 = 0 ']'
++ echo OK
+OK
 exit $res
++ exit 0
