0ee21224c2ec00d28a7230b33a62731998027bc8
ikki yamanaka <ikki.yama@icloud.com>
2025-12-31 17:37:23 +0900

    add

--- test.bash
+++ test.bash
@@ -9,21 +9,19 @@ ng () {
     NG_COUNT=$((NG_COUNT + 1))
 }
 
-FUTURE_DATE="2099-12-31"
-OUT=$(echo "${FUTURE_DATE}" | ./count | tr -d ' ' | tr -d '\n')
-[ "${OUT}" = "error" ] || ng "$LINENO"
+echo "2000-01-01" | ./count > /dev/null 2>&1
+[ $? -eq 0 ] || ng "$LINENO"
 
-OUT=$(echo "2024/01/01" | ./count | tr -d ' ' | tr -d '\n')
-[ "${OUT}" = "error" ] || ng "$LINENO"
+echo "2099-12-31" | ./count > /dev/null 2>&1
+[ $? -eq 1 ] || ng "$LINENO"
 
-OUT=$(echo "2024-02-30" | ./count | tr -d ' ' | tr -d '\n')
-[ "${OUT}" = "error" ] || ng "$LINENO"
+echo "2024/01/01" | ./count > /dev/null 2>&1
+[ $? -eq 1 ] || ng "$LINENO"
 
 if [ "${NG_COUNT}" -eq 0 ]; then
-    echo "OK"
+    echo "0"
     exit 0
 else
-    echo "NG: ${NG_COUNT}件の失敗"
+    echo "${NG_COUNT}"
     exit 1
 fi
-
